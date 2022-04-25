3f54c7c0a26043896ac77168cf698dc8d9cf593f
nurovic <mertnurovic@gmail.com>
2022-04-25 23:56:44 +0300

    Error Handle

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
     name:{
         type: String,
-        require: [true, 'Please enter product name'],
+        required: [true, 'Please enter product name'],
         trim: true,
         maxlength:[100, 'Product name cannot exceed 100 characters']
     },
     price:{
         type: Number,
-        require: [true, 'Please enter product name'],
+        required: [true, 'Please enter product name'],
         maxlength:[5, 'Product name cannot exceed 5 characters'],
         default: 0.0
     },
     description:{
         type: String,
-        require: [true, 'Please enter product description'],
+        required: [true, 'Please enter product description'],
     },
     ratings: {
         type: Number,
