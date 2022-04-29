744a0b35ee8252eb7532dfd50f19ec01fd920f23
nurovic <mertnurovic@gmail.com>
2022-04-29 16:46:07 +0300

    nodeMailler

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -18,20 +18,38 @@ module.exports = (err, req, res, next) => {
     error.message = err.message;
 
     // Wrong Mongoose Object ID Error
-    if(err.name == 'CastError') {
-        const message = `Resource not found. Invalid ${err.path}`
-        error = new ErrorHandler(message, 400)
-    }  
+    if (err.name == "CastError") {
+      const message = `Resource not found. Invalid ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
 
     // Handling Mongoose Validation Error
-    if(err.name == 'ValidationError') {
-        const message = Object.values(err.errors).map(value => value.message)
-        error = new ErrorHandler(message, 400)
+    if (err.name == "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
     }
+
+    // Handling Mongoose duplicate key errors
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handlng wrong JWT error
+    if (err.name == "JsonWebTokenError") {
+      const message = "JSON web Token is invalid. Try Again!!!"
+      error = new ErrorHandler(message, 400);
+    }
+    // Handlng wrong JWT error
+    if (err.name == "TokenExpiredError") {
+      const message = "JSON web Token is expired. Try Again!!!"
+      error = new ErrorHandler(message, 400);
+    }
+
 
     res.status(error.statusCode).json({
       success: false,
-      message: error.message || 'Internal Server Error'
+      message: error.message || "Internal Server Error",
     });
-  } 
+  }
 };
