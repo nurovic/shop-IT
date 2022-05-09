a0769271b56cd4c4e4588de27b3f0e3c793c02d6
nurovic <mertnurovic@gmail.com>
2022-05-09 23:56:39 +0300

    product Detail

--- frontend/src/actions/productsActions.js
+++ frontend/src/actions/productsActions.js
@@ -42,7 +42,7 @@ export const getProductDetails = (id) => async (dispatch) => {
         })
     }
 }
-export const clearErros = () => async (dispatch) => {
+export const clearErrors = () => async (dispatch) => {
     dispatch({
         type: CLEAR_ERRORS
     })
