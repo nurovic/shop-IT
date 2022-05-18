ce9e6402094a14421c717d36fd79b5721bebbb8b
nurovic <mertnurovic@gmail.com>
2022-05-18 15:00:01 +0300

    Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/layout/Footer";
 
 import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
+import Login from './components/user/Login';
+
 function App() {
   return (
     <Router>
@@ -15,6 +17,7 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home}/>
           <Route path="/product/:id" component={ProductDetails} exact  />
+          <Route path="/login" component={Login}   />
 
       </div>
       <Footer />
