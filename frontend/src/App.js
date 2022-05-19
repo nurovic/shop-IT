f36246e771402fcf1b5917069266ca0e579555d7
nurovic <mertnurovic@gmail.com>
2022-05-19 18:41:39 +0300

    load login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from "./components/layout/Header";
@@ -7,7 +8,17 @@ import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from './components/user/Login';
 
+import Register from './components/user/Register';
+
+import { loadUser } from './actions/userActions'
+import store from './store'
+
 function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
   return (
     <Router>
 
@@ -17,7 +28,9 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home}/>
           <Route path="/product/:id" component={ProductDetails} exact  />
-          <Route path="/login" component={Login}   />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+
 
       </div>
       <Footer />
