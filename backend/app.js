9d8f957bfc8cfd58a9948a93a7f5a378b7287091
Mert <85342773+nurovic@users.noreply.github.com>
2022-07-03 00:22:12 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use('/api/v1', order)
 app.use('/api/v1', payment)
 
 if(process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(___dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
